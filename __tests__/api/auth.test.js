e97859ac286178b9d912189b5bf316080e61af65
Eric Stanley <eric.stanleyjones@gmail.com>
2022-02-17 23:51:20 +0530

    Added additional tests for authentication

--- __tests__/api/auth.test.js
+++ __tests__/api/auth.test.js
@@ -10,32 +10,88 @@ afterAll(async () => {
   await db.disconnect();
 });
 
-test('should signup a new user', async () => {
-  await request(app)
-    .post('/api/v1/auth/signup')
-    .send({
-      fields: {
-        input: {
-          username: 'fuzzyspuffy',
-          password: 'EnoB6533+',
-          password_confirm: 'EnoB6533+',
-          email: 'fuzzyspuffy@test.com',
-        },
-      },
-    })
-    .expect(201);
+const newUser = {
+  fields: {
+    input: {
+      username: 'fuzzyspuffy',
+      password: 'EnoB6533+',
+      password_confirm: 'EnoB6533+',
+      email: 'fuzzyspuffy@test.com',
+    },
+  },
+};
+
+const user = {
+  fields: {
+    input: {
+      password: 'EnoB6533+',
+      email: 'fuzzyspuffy@test.com',
+    },
+  },
+};
+
+describe('POST /api/v1/auth/signup', () => {
+  describe('given a new user signup fields', () => {
+    test('should respond with a 201 status code', async () => {
+      const response = await request(app)
+        .post('/api/v1/auth/signup')
+        .send(newUser);
+      expect(response.statusCode).toBe(201);
+      expect(response.headers['content-type']).toEqual(
+        expect.stringContaining('json')
+      );
+    });
+  });
+
+  describe('given an existing user signup fields', () => {
+    test('should respond with a 500 status code', async () => {
+      const response = await request(app)
+        .post('/api/v1/auth/signup')
+        .send(newUser);
+      expect(response.statusCode).toBe(500);
+      expect(response.headers['content-type']).toEqual(
+        expect.stringContaining('json')
+      );
+    });
+  });
 });
 
-test('should login an existing user', async () => {
-  await request(app)
-    .post('/api/v1/auth/login')
-    .send({
-      fields: {
-        input: {
-          password: 'EnoB6533+',
-          email: 'fuzzyspuffy@test.com',
-        },
-      },
-    })
-    .expect(200);
+describe('POST /api/v1/auth/login', () => {
+  describe('given a email and password', () => {
+    test('should respond with a 200 status code', async () => {
+      const response = await request(app).post('/api/v1/auth/login').send(user);
+      expect(response.statusCode).toBe(200);
+    });
+    test('should specify json in the content type header', async () => {
+      const response = await request(app).post('/api/v1/auth/login').send(user);
+      expect(response.headers['content-type']).toEqual(
+        expect.stringContaining('json')
+      );
+    });
+    test('response has token', async () => {
+      const response = await request(app).post('/api/v1/auth/login').send(user);
+      expect(response.body.token).toBeDefined();
+    });
+  });
+
+  describe('when the username and password is missing', () => {
+    test('should respond with a status code of 400', async () => {
+      const bodyData = [
+        { fields: { input: { email: 'email' } } },
+        { fields: { input: { password: 'password' } } },
+        { fields: { input: {} } },
+      ];
+      for (const body of bodyData) {
+        const response = await request(app)
+          .post('/api/v1/auth/login')
+          .send(body);
+        expect(response.statusCode).toBe(400);
+      }
+    });
+
+    test('should respond with a status code of 500', async () => {
+      const response = await request(app).post('/api/v1/auth/login').send({});
+      expect(response.statusCode).toBe(500);
+    });
+  });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   testSequencer: './__tests__/customSequencer.js',
   testEnvironment: 'node',
-  testMatch: ['**/**/*.test.js'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
   verbose: true,
   forceExit: false,
   clearMocks: true,
@@ -9,6 +12,39 @@ module.exports = {
   restoreMocks: true,
   detectOpenHandles: true,
   testTimeout: 3000,
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  coverageDirectory: '<rootDir>/reports/coverage/',
+  coverageReporters: ['html', 'text'],
+  reporters: [
+    'default',
+    [
+      'jest-stare',
+      {
+        resultDir: 'reports/jest-stare',
+        reportTitle: 'jest-stare!',
+        additionalResultsProcessors: ['jest-junit'],
+        coverageLink: '../coverage/index.html',
+        jestStareConfigJson: 'jest-stare.json',
+        jestGlobalConfigJson: 'globalStuff.json',
+      },
+    ],
+    [
+      'jest-html-reporters',
+      {
+        publicPath: './reports/jest-html-reporters',
+        filename: 'report.html',
+        openReport: false,
+      },
+    ],
+  ],
   projects: [
     {
       rootDir: '<rootDir>/__tests__/unit',
